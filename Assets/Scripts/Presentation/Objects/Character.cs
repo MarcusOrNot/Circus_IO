9deb50a2fff348faf0f7d2f82bffa6eb31aa8506
Sergo12312 <149957454+Sergo12312@users.noreply.github.com>
2023-11-10 17:52:06 +0300

    Add some fixes

--- Assets/Scripts/Presentation/Objects/Character.cs
+++ Assets/Scripts/Presentation/Objects/Character.cs
@@ -8,17 +8,17 @@ public class Character : MonoBehaviour
        
 
     public void Move(Vector2 direction)
-    {
+    {        
         if ((direction.magnitude < _model.SensivityTreshold) || (Mathf.Abs(_rigidbody.velocity.y) > 1f)) 
-        { 
-            _rigidbody.velocity = new Vector3(0 , _rigidbody.velocity.y, 0);
+        {                         
             _isMoving = false;
             return; 
         }        
         Quaternion targetRotation = transform.rotation * Quaternion.AngleAxis(Vector3.SignedAngle(transform.forward, new Vector3(direction.x, 0, direction.y), Vector3.up), Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _model.RotationSpeed * Time.deltaTime);
         if (transform.rotation == targetRotation)
-        {            
+        {
+            if (!_isMoving) _rigidbody.constraints &= ~ (RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ);            
             _isMoving = true;
             if (!_isStoppingProcessStart) StartCoroutine(StopMoving()); 
             direction *= _model.Speed * _speedMultiplier;
@@ -37,9 +37,13 @@ public class Character : MonoBehaviour
     private bool _isMoving = false;
     private bool _isStoppingProcessStart = false;
 
+    private bool _isInCollision = false;
+
     private void Awake()
     {        
         _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.constraints = RigidbodyConstraints.FreezeAll & ~RigidbodyConstraints.FreezePositionY;
+        
     }  
 
     private IEnumerator StopMoving()
@@ -50,8 +54,29 @@ public class Character : MonoBehaviour
             _isMoving = false;
             yield return new WaitForSeconds(Mathf.Max(Time.deltaTime, Time.fixedDeltaTime) * 2);                       
         }
+        _rigidbody.constraints |= RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ; 
         Move(Vector2.zero);
         _isStoppingProcessStart = false;
-    }    
+    }
+        
 
+    private void OnCollisionEnter()
+    {      
+        _rigidbody.constraints |= RigidbodyConstraints.FreezePositionY;
+        StartCoroutine(CollisionExitTimer());
+    }
+     
+    private void OnCollisionExit()
+    {
+        _rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionY;
+    }
+
+    private IEnumerator CollisionExitTimer()
+    {
+        if (_isInCollision) yield break;
+        _isInCollision = true;
+        yield return new WaitForSeconds(0.1f);
+        OnCollisionExit();
+        _isInCollision = false;
+    }
 }
