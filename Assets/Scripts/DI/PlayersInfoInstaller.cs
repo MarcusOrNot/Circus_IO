e9a095a4e4f045c39b9959453454318a3b074e4a
force <mirconote15@gmail.com>
2023-11-27 20:51:01 +0300

    Added player info system

---
+++ Assets/Scripts/DI/PlayersInfoInstaller.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayersInfoInstaller : MonoInstaller
+{
+    [SerializeField] private List<PlayerInfoModel> _playersInfo;
+    public override void InstallBindings()
+    {
+        
+    }
+}
