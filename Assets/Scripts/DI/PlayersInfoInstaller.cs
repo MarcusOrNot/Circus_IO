33a7a0f4c6abc6b8fdd5eb4e48687a776ff372bd
force <mirconote15@gmail.com>
2023-11-29 18:00:29 +0300

    Added hud player

--- Assets/Scripts/DI/PlayersInfoInstaller.cs
+++ Assets/Scripts/DI/PlayersInfoInstaller.cs
@@ -7,6 +7,7 @@ public class PlayersInfoInstaller : MonoInstaller
     [SerializeField] private List<PlayerInfoModel> _playersInfo;
     public override void InstallBindings()
     {
-        
+        Container.Bind<PlayersInfoService>().FromNew().AsSingle().Lazy();
+        Container.BindInstances(_playersInfo);
     }
 }
--- Assets/Scripts/Presentation/Controllers/PlayerHUDController.cs
+++ Assets/Scripts/Presentation/Controllers/PlayerHUDController.cs
@@ -1,24 +1,44 @@
+using AppodealStack.Monetization.Common;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 public class PlayerHUDController : MonoBehaviour
 {
+    [SerializeField] private Image _playerImage;
+    [SerializeField] private TextMeshProUGUI _playerName;
+    [Inject] private PlayersInfoService _playersInfoService;
+    private const float OFFSET = -50;
     private Hunter _hunter;
-    private void Awake()
-    {
-        _hunter = GetComponentInParent<Hunter>();
-    }
+    private RectTransform _canvasRect = null;
+    private Camera _camera = null;
+    private PlayerInfoModel _playerInfoModel;
     // Start is called before the first frame update
     void Start()
     {
-        //GetComponent<Canvas>().worldCamera = Camera.main;
-        gameObject.transform.parent = Camera.main.transform;
+        _hunter = GetComponentInParent<Hunter>();
+        _canvasRect = Level.Instance.GetHUDCanvas?.GetComponent<RectTransform>();
+        _camera = Camera.main;
+        if (_hunter== null || _camera == null || _canvasRect == null) Destroy(gameObject);
+        gameObject.transform.parent = _canvasRect;
+        _playerInfoModel = _playersInfoService.GeneratePlayerInfo();
+        _playerImage.sprite = _playerInfoModel.PlayerIcon;
+        _playerName.text = _playerInfoModel.PlayerName;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = _hunter.transform.position;
+        if (_hunter==null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        var viewPos = _camera.WorldToViewportPoint(_hunter.transform.position);
+        Vector2 finalPosition = new Vector2(viewPos.x * _canvasRect.sizeDelta.x, viewPos.y * _canvasRect.sizeDelta.y+OFFSET);
+        GetComponent<RectTransform>().anchoredPosition = finalPosition;
     }
 }
