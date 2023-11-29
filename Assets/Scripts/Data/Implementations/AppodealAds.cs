d9d8b566664afd9ba6f6090874ac51f35b92167a
force <mirconote15@gmail.com>
2023-11-29 10:27:12 +0300

    Added ads info

---
+++ Assets/Scripts/Data/Implementations/AppodealAds.cs
@@ -0,0 +1,65 @@
+using AppodealStack.Monetization.Api;
+using AppodealStack.Monetization.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppodealAds : MonoBehaviour, IAds
+{
+    private Action<bool> _onShowInterstitial;
+    private Action<bool> _onShowRewarded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int adTypes = AppodealAdType.Interstitial | AppodealAdType.Banner | AppodealAdType.RewardedVideo;
+        string appKey = "YOUR_APPODEAL_APP_KEY";
+        //AppodealCallbacks.Sdk.OnInitialized += OnInitializationFinished;
+        //Interstitial Callbacks
+        AppodealCallbacks.Interstitial.OnShowFailed += delegate { _onShowInterstitial?.Invoke(false); };
+        AppodealCallbacks.Interstitial.OnClosed += delegate { _onShowInterstitial?.Invoke(true); };
+        AppodealCallbacks.RewardedVideo.OnShowFailed += delegate { _onShowRewarded?.Invoke(false); };
+        AppodealCallbacks.RewardedVideo.OnClosed += RewardedVideo_OnClosed;
+        //Rewarded Callbacks
+        
+        Appodeal.Initialize(appKey, adTypes);
+    }
+
+    private void RewardedVideo_OnClosed(object sender, RewardedVideoClosedEventArgs e)
+    {
+        _onShowRewarded?.Invoke(e.Finished);
+    }
+
+    public void HideBanner()
+    {
+        Appodeal.Hide(AppodealAdType.Banner);
+    }
+
+    public void ShowBanner()
+    {
+        Appodeal.Show(AppodealShowStyle.BannerBottom);
+    }
+
+    public bool ShowInterstitialAd(Action<bool> onShowInterstitial)
+    {
+        if (Appodeal.IsLoaded(AppodealAdType.Interstitial))
+        {
+            _onShowInterstitial = onShowInterstitial;
+            Appodeal.Show(AppodealShowStyle.Interstitial);
+            return true;
+        }
+        return false;
+    }
+
+    public bool ShowRewardedAd(Action<bool> onShowRewarded)
+    {
+        if (Appodeal.IsLoaded(AppodealAdType.RewardedVideo))
+        {
+            _onShowRewarded = onShowRewarded;
+            Appodeal.Show(AppodealShowStyle.RewardedVideo);
+            return true;
+        }
+        return false;
+    }
+}
--- Assets/Scripts/Presentation/Controllers/PlayerHUDController.cs
+++ Assets/Scripts/Presentation/Controllers/PlayerHUDController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerHUDController : MonoBehaviour
 {
+    private Hunter _hunter;
+    private void Awake()
+    {
+        _hunter = GetComponentInParent<Hunter>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Canvas>().worldCamera = Camera.main;
+        //GetComponent<Canvas>().worldCamera = Camera.main;
+        gameObject.transform.parent = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = _hunter.transform.position;
     }
 }
