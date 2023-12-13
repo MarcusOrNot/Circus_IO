083bfc78edeb95ed54b9dc7c68e8ea41872a830c
force <mirconote15@gmail.com>
2023-12-13 10:26:50 +0300

    Added Hat Factory

--- Assets/Scripts/DI/ObjectInstaller.cs
+++ Assets/Scripts/DI/ObjectInstaller.cs
@@ -6,6 +6,7 @@ public class ObjectInstaller : MonoInstaller
 {
     [SerializeField] private List<Entity> _entities;
     [SerializeField] private List<Booster> _boosters;
+    [SerializeField] private List<Hat> _hats;
     public override void InstallBindings()
     {
         Container.Bind<EntityFactory>().FromNew().AsSingle();
@@ -13,5 +14,8 @@ public class ObjectInstaller : MonoInstaller
 
         Container.Bind<BoosterFactory>().FromNew().AsSingle();
         Container.BindInstance(_boosters);
+
+        Container.Bind<HatFactory>().FromNew().AsSingle();
+        Container.BindInstance(_hats);
     }
 }
