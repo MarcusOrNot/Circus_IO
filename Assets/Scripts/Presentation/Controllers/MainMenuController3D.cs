7284cbdf672e0b1042efc57aa93d226b9b1d3e9e
force <mirconote15@gmail.com>
2023-12-19 20:49:40 +0300

    Shop Hat Mods

--- Assets/Scripts/Presentation/Controllers/MainMenuController3D.cs
+++ Assets/Scripts/Presentation/Controllers/MainMenuController3D.cs
@@ -4,12 +4,26 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
+using DG.Tweening;
+using System;
 
 public class MainMenuController3D : MonoBehaviour
 {
     [Inject] private IGameStats _gameStats;
+    [SerializeField] private MainMenuPanelController _mainMenu;
+    [SerializeField] private ShopHatController _shopHatMenu;
     [SerializeField] private StartGameUI _startGameUI;
     [SerializeField] private TextMeshProUGUI _coinsCountValue;
+    private Camera _camera;
+    //private Transform _cameraStartTransform;
+    private Vector3 _cameraStartPosition;
+    //private Transform _mainTransformDelta;
+    //private Transform _shopHatTransformDelta;
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _cameraStartPosition = _camera.transform.position;
+    }
     private void Start()
     {
         _coinsCountValue.text = _gameStats.GetStat(GameStatsType.COINS).ToString();
@@ -25,8 +39,27 @@ public class MainMenuController3D : MonoBehaviour
     {
         Application.Quit();
     }
-    public void ShowNextHat()
+    public void ShowMainMenu()
     {
-
+        //HideMenus();
+        ShowMenuAnim(new Vector3(0, 0, 0), () => _mainMenu.gameObject.SetActive(true));
+        //_mainMenu.gameObject.SetActive(true);
+    }
+    public void ShowHatsShopMenu()
+    {
+        //HideMenus();
+        ShowMenuAnim(new Vector3(-2.5f,0,0), ()=> _shopHatMenu.gameObject.SetActive(true));
+        //_shopHatMenu.gameObject.SetActive(true);
+    }
+    private void HideMenus()
+    {
+        _mainMenu.gameObject.SetActive(false);
+        _shopHatMenu.gameObject.SetActive(false);
+    }
+    private void ShowMenuAnim(Vector3 deltaPos, Action onEnd)
+    {
+        HideMenus();
+        var sequence = DOTween.Sequence();
+        sequence.Append(_camera.transform.DOLocalMove(_cameraStartPosition+ deltaPos, 1.0f)).OnComplete(()=>onEnd?.Invoke());
     }
 }
--- Assets/Scripts/Presentation/UI/Panels/ShopHatController.cs
+++ Assets/Scripts/Presentation/UI/Panels/ShopHatController.cs
@@ -6,9 +6,11 @@ using Zenject;
 public class ShopHatController : MonoBehaviour
 {
     //[SerializeField] private GameObject _menuHunters;
+    [Inject] private ISettings _settings;
     [Inject] private ItemsProgressService _progress;
     [Inject] private HatShopFactory _hatsFactory;
     [SerializeField] private Transform _hatsPlace;
+    [SerializeField] private BubbleForm _hatObject;
     private List<HatShopItem> _hats = new List<HatShopItem>();
     private int _selectedHunter = 0;
     private int _currentHat = 0;
@@ -18,7 +20,7 @@ public class ShopHatController : MonoBehaviour
         foreach (var hat in hats)
         {
             _hats.Add(hat);
-            hat.transform.parent = _hatsPlace;
+            hat.transform.SetParent(_hatsPlace);
             hat.SetOnClicked(() =>
             {
                 foreach (var item in _hats)
@@ -26,6 +28,8 @@ public class ShopHatController : MonoBehaviour
                     if (item != hat)
                         item.Selected = false;
                 }
+                _settings.ChosenHat = hat.Model.Hat;
+                _hatObject.SetHat(hat.Model.Hat);
             });
         }
     }
