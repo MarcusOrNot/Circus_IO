d0b19bea841d716ebbd3a9c5f6a8ff2a7803ca22
force <mirconote15@gmail.com>
2024-01-23 21:19:40 +0300

    Added simple locale package

---
+++ Assets/SimpleLocalization/Scripts/LocalizedTextMeshPro.cs
@@ -0,0 +1,32 @@
+//using Assets.SimpleLocalization.Scripts;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.SimpleLocalization.Scripts
+{
+    /// <summary>
+	/// Localize text component.
+	/// </summary>
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizedTextMeshPro : MonoBehaviour
+    {
+        public string LocalizationKey;
+
+        public void Start()
+        {
+            Localize();
+            LocalizationManager.OnLocalizationChanged += Localize;
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.OnLocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+            GetComponent<TextMeshProUGUI>().text = LocalizationManager.Localize(LocalizationKey);
+        }
+    }
+}
